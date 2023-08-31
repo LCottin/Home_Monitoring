a7bffb5a8de84ff96a58fe0f3e1020e90782ec0f
LCottin <cottin_leonard@icloud.com>
2023-08-31 20:05:12 +0200

    Added instant data in living room

---
+++ WebSite/App/Routes/LivingRoom/instantData.js
@@ -0,0 +1,35 @@
+const express   = require("express");
+const path      = require("path");
+const dotv      = require("dotenv").config();
+const router    = express.Router();
+const mysql     = require('mysql');
+
+// Créer une connexion à la base de données MySQL
+const connection = mysql.createConnection({
+    host: process.env.HOST_DB,
+    user: process.env.USER_DB,
+    password: process.env.PASS_DB,
+    database: process.env.NAME_DB
+});
+
+// Se connecter à la base de données
+connection.connect();
+
+router.get("/", function (req, res) {
+    connection.query('SELECT * FROM LivingRoom ORDER BY id DESC LIMIT 1', function (error, results, fields) {
+        if (error)
+            throw error;
+
+        const date = new Date(results[0].time * 1000);
+
+        // Renvoyer les données au format JSON	
+        res.render(path.resolve("Public/ejs/LivingRoom/instantData.ejs"), {
+            timestamp: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes(),
+            temperature: results[0].temperature,
+            humidity: results[0].humidity,
+            pressure: results[0].pressure,
+        });
+    });
+});
+
+module.exports = router;
--- WebSite/WebSite.js
+++ WebSite/WebSite.js
@@ -6,10 +6,18 @@ const app 		 = express();
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 8080;
 
+// Importing routes
+const LivingRoom = require("./App/Routes/LivingRoom/instantData");
+
 // Setting up the static files
 app.use(express.static(__dirname + "/Public/css"));
+app.use(express.static(__dirname + "/src/LivingRoom"));
+app.use(express.static(__dirname + "/Public/img"));
 
-// Setting up the route
+// Setting up the routes
+app.use("/livingroom", LivingRoom);
+
+// Setting up the entry point
 app.get("/", function (req, res)
 {
 	res.sendFile(__dirname + "/Public/html/index.html");
