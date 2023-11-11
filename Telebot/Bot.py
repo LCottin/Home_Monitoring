b10c762a4797e1c5345a0d8b0e4ba172920bb8df
LCottin <cottin_leonard@icloud.com>
2023-11-11 16:56:22 +0100

    Updated bot

--- Telebot/Bot.py
+++ Telebot/Bot.py
@@ -1,68 +1,84 @@
 import telebot
 from env import TOKEN, CHAT_ID
-from time import sleep
+from Database import Database
+from signal import signal, SIGINT
 from datetime import datetime
+import os
 
 bot = telebot.TeleBot(TOKEN)
+
 class Bot:
-	def __init__(self, database):
-		self.token 		= TOKEN
-		self.chat_id 	= CHAT_ID
-		self.bot 	    = bot
-		self.database   = database
+    def __init__(self, database):
+        self.token 		= TOKEN
+        self.chat_id 	= CHAT_ID
+        self.bot 	    = bot
+        self.database   = database
+        self.is_stopped = False
+        self.updater    = None
+        self.running    = False
 
-		"""
-		Send a message when the command /start or /help is issued.
-		"""
-		@bot.message_handler(commands=['help', 'start'])
-		def send_welcome(message):
-			msg = self.reply_to(message, "Welcome !")
+        """
+        Send a message when the command /start or /help is issued.
+        """
+        @bot.message_handler(commands=['help', 'start'])
+        def send_welcome(message):
+            msg = self.reply_to(message, "Welcome !")
 
-		"""
-		Send last data when the command /last is issued.
-		"""
-		@bot.message_handler(commands=['last'])
-		def send_last(message):
-			lastData   = self.database.getLastData()
-			time 	   = datetime.fromtimestamp(lastData["time"]).strftime("%d/%m/%Y %H:%M:%S")
-			msgToSend  = "Last data:\n"
-			msgToSend += "\tTime: " + time + "\n"
-			msgToSend += "\tTemperature: " + str(lastData["temperature"]) + " °C\n"
-			msgToSend += "\tHumidity: " + str(lastData["humidity"]) + " %\n"
-			msgToSend += "\tPressure: " + str(lastData["pressure"]) + " hPa\n"
-			msgToSend += "\tAltitude: " + str(lastData["altitude"]) + " m\n"
-			msgToSend += "\tGas resistance: " + str(lastData["gas_resistance"]) + " Ohm\n"
-			self.reply_to(message, msgToSend)
-		
-		print("Bot initialized")
+        """
+        Send last data when the command /last is issued.
+        """
+        @bot.message_handler(commands=['last'])
+        def send_last(message):
+            lastData   = self.database.getLastData()
+            time 	   = datetime.fromtimestamp(lastData["time"]).strftime("%d/%m/%Y %H:%M:%S")
+            msgToSend  = "Last data:\n"
+            msgToSend += "\tTime: " + time + "\n"
+            msgToSend += "\tTemperature: " + str(lastData["temperature"]) + " °C\n"
+            msgToSend += "\tHumidity: " + str(lastData["humidity"]) + " %\n"
+            msgToSend += "\tPressure: " + str(lastData["pressure"]) + " hPa\n"
+            msgToSend += "\tAltitude: " + str(lastData["altitude"]) + " m\n"
+            msgToSend += "\tGas resistance: " + str(lastData["gas_resistance"]) + " Ohm\n"
+            self.reply_to(message, msgToSend)
 
-	def send_message(self, message):
-		self.bot.send_message(self.chat_id, message)
+        """
+        Stop the bot when the command /stop is issued.
+        """
+        @bot.message_handler(commands=['stop'])
+        def send_stop(message):
+            self.reply_to(message, "Bot stopped")
+            self.stop()
 
-	def reply_to(self, message, text):
-		self.bot.reply_to(message, text)
-	
-	def get_updates(self):
-		return self.bot.get_updates()
+        print("Bot initialized")
 
-	def get_last_update(self):
-		return self.bot.get_updates()[-1]
+    def send_message(self, message):
+        self.bot.send_message(self.chat_id, message)
 
-	def get_last_message(self):
-		return self.get_last_update().message
+    def reply_to(self, message, text):
+        self.bot.reply_to(message, text)
 
-	def get_last_message_text(self):
-		return self.get_last_message().text
+    def is_running(self):
+        return not self.is_stopped
 
-	def get_last_message_date(self):
-		return self.get_last_message().date
+    def run(self):
+        self.is_stopped = False
+        if self.running == False:
+            self.running = True
+            bot.infinity_polling(none_stop=True, interval=0)
+            print("Bot running")
 
-	def run(self):
-		print("Bot running")
-		self.bot.infinity_polling()
-
+    def stop(self, *args, **kwargs):
+        self.is_stopped = True
+        self.running = False
+        print("Bot not running")
 
 if __name__ == '__main__':
-	myBot = Bot()
-	myBot.run()
-		
+    db = Database()
+    myBot = Bot(db)
+
+    # Register a signal handler to stop the bot gracefully
+    signal(SIGINT, myBot.stop)
+
+    try:
+        myBot.run()
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- Telebot/main.py
+++ Telebot/main.py
@@ -1,17 +1,26 @@
 from Bot import Bot
 from Database import Database
 from env import TOKEN, CHAT_ID
+from signal import signal, SIGINT
+import sys
 
 def main():
 
     db = Database()
     bot = Bot(db)
 
-    while True:
+    # Register a signal handler to stop the bot gracefully
+    signal(SIGINT, bot.stop)
+
+    while bot.is_running():
         try:
             bot.run()
+        except KeyboardInterrupt:
+            # Handle Ctrl+C (SIGINT)
+            break
         except Exception as e:
-            print(e)
+            print(f"An error occurred: {e}")
+            break
 
 
 if __name__ == '__main__':
